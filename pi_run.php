05fa65a8dc1dc4f96ad645ee403ed2e574380096
Konstantin <kfranchuk@kfranchuk>
2019-03-27 15:04:42 +0300

    added dynamic server name load

--- pi_run.php
+++ pi_run.php
@@ -2,9 +2,11 @@
 
 require_once(__DIR__ .'/init.php');
 
+$uploadScript = file_get_contents('uploadScript.txt');
+
 try {
     $camera = new Classes\Camera();
-    $server = new Classes\Pi("");
+    $server = new Classes\Pi($uploadScript);
     $fileName = $camera->makeShot();
     $server->sendToServer($fileName);
 } catch (\Exception $e) {
